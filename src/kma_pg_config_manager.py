2f728b3112ca69361deacad7a5c36039d183cece
Michael BAG <mk@remark.pro>
2025-11-06 09:42:55 +0300

    Bump versions: config_manager 1.1.3, config_setup 1.1.4, project 2.0.8

--- src/kma_pg_config_manager.py
+++ src/kma_pg_config_manager.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 PostgreSQL Backup Manager - Configuration Manager
-Version: 2.0.7/1.0.0
+Version: 2.0.8/1.1.3
 Author: Michael BAG
 Email: mk@remark.pro
 Telegram: https://t.me/michaelbag
